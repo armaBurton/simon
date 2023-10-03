972c9d86ef399940254630831aa0ab3623960f10
Arma Burton <arma@armaburton.com>
2023-10-03 10:49:43 -0700

    repositioned the src folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 import './App.css';
+import { Layout } from './components/Layout/Layout';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+      </Route>
+    )
+  );
 
+  return <RouterProvider router={router} />;
 }
 
 export default App;
