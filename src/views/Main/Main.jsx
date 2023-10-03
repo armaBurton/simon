ac23588fb6055635294add8238763b7651f343f0
Arma Burton <arma@armaburton.com>
2023-10-03 13:07:45 -0700

    buttons have interacivity

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -3,10 +3,10 @@ import "./Main.css";
 export const Main = () => {
     return <section className="main-section">
         <div className="simon-body">
-            <div className="button yellow" />
-            <div className="button red" />
-            <div className="button blue" />
-            <div className="button green" />
+            <div className="game-button yellow" />
+            <div className="game-button red" />
+            <div className="game-button blue" />
+            <div className="game-button green" />
             <div className="center" />
         </div>
     </section>;
