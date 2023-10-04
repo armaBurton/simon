ebcc248f8c06cd68c0fb658ffa5a322aa5e3b87f
Arma Burton <arma@armaburton.com>
2023-10-04 09:46:50 -0700

    added a click handler for yellow button

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,13 +1,28 @@
 import "./Main.css";
+import { useState } from "react";
+
+
 
 export const Main = () => {
+    const [count, setCount] = useState("000");
+
+    const handleClick = () => {
+        alert("click");
+    }
+
     return <section className="main-section">
         <div className="simon-body">
-            <div className="game-button yellow" />
+            <div className="game-button yellow" onclick={handleClick} />
             <div className="game-button red" />
             <div className="game-button blue" />
             <div className="game-button green" />
-            <div className="center" />
+            <div className="center" >
+                <h1 className="title">simon</h1>
+                <div className="score-container">
+                    <h2 className="score-label">SCORE</h2>
+                    <div className="score-counter"><p className="count">{count}</p></div>
+                </div>
+            </div>
         </div>
     </section>;
 }
