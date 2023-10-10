2faa39af37127dc2ba0e5236676d9e7520832843
Arma Burton <arma@armaburton.com>
2023-10-09 20:46:01 -0700

    working on setTimeout, which doesn't seem to be working as I expect it to

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -10,56 +10,77 @@ import { greenButton } from "../../components/buttons/greenButton";
 export const Main = () => {
     const [count, setCount] = useState(0);
     const [randoPattern, setRandoPattern] = useState([]);
-    const [userPattern, setUserPatter] = useState([]);
+    const [userPattern, setUserPattern] = useState([]);
     const [gameOn, setGameOn] = useState(0);
+    const [playerTurn, setPlayerTurn] = useState(0)
 
     const handleStart = () => {
         setGameOn(1);
-        playGame();
     }
 
     const handleStop = () => {
         setGameOn(0);
     }
 
-    // useEffect(() => {
-    //     if (gameOn === 1) {
-    //         setCount(0);
-    //         playGame();
-    //     }
-    // }, [gameOn])
-
-    const playGame = () => {
-        const hotSwap = [];
-        for (let i = 0; i < 10; i++) {
-            const randoNumbo = Math.floor(Math.random() * (4) + 1)
-            hotSwap.push(randoNumbo);
-            setRandoPattern(hotSwap);
+    useEffect(() => {
+        if (gameOn === 1) {
+            setRandoPattern([]);
+            setUserPattern([]);
+            setCount(0);
+            randoNumboGenerato(randoPattern);
+            playSequence(randoPattern);
         }
-        // do {
-        //     const randoNumbo = Math.floor(Math.random() * (4) + 1)
-        //     hotSwap.push(randoNumbo);
-        //     setRandoPattern(hotSwap);
-        //     console.log(randoPattern);
-        //     // if (hotSwap.length() === 10) setGameOn(0);
-        //     setGameOn(gameOn + 1);
-        // } while (gameOn < 10);
+    }, [gameOn])
+
+    // useEffect(() => {
+    // }, [userPattern])
+
+    // useEffect(() => {
+    // }, [randoPattern])
+
+    const randoNumboGenerato = (arr) => {
+        setRandoPattern([...arr, Math.ceil(Math.random() * 4)])
     }
+
+    const playSequence = (sequence) => {
+        for (let i = 0; i < sequence.length; i++) {
+            const temp = i;
+            setTimeout(() => console.log(randoPattern[temp]), 5000);
+            // timeOut(i);
+        }
+    }
+
+    // const timeOut = (i) => {
+    // }
+
+    // const playGame = () => {
+    //     if (playerTurn === 0) {
+    //         const randoNumbo = Math.floor(Math.random() * (4) + 1)
+    //         setRandoPattern([...randoPattern, randoNumbo]);
+    //     } else if (playerTurn === 1 && userPattern < randoPattern) {
+
+    //     }
+
+    // }
 
     const handleYellowClick = () => {
         setCount(yellowButton(count));
+        setUserPattern([...userPattern, 1]);
     }
 
     const handleRedClick = () => {
         setCount(redButton(count));
+        setUserPattern([...userPattern, 2]);
     }
 
     const handleBlueClick = () => {
         setCount(blueButton(count));
+        setUserPattern([...userPattern, 3]);
     }
 
     const handleGreenClick = () => {
         setCount(greenButton(count));
+        setUserPattern([...userPattern, 4]);
     }
 
     return <section className="main-section">
@@ -68,10 +89,10 @@ export const Main = () => {
             <button className="reset-button control-buttons" onClick={handleStop}>stop</button>
         </div>
         <div className="simon-body">
-            <div className="game-button yellow" onClick={handleYellowClick} />
-            <div className="game-button red" onClick={handleRedClick} />
-            <div className="game-button blue" onClick={handleBlueClick} />
-            <div className="game-button green" onClick={handleGreenClick} />
+            <div className="game-button yellow" data-tile="yellow" onClick={handleYellowClick} />
+            <div className="game-button red" data-tile="red" onClick={handleRedClick} />
+            <div className="game-button blue" data-tile="blue" onClick={handleBlueClick} />
+            <div className="game-button green" data-tile="green" onClick={handleGreenClick} />
             <div className="center" >
                 <h1 className="title">simon</h1>
                 <div className="score-container">
