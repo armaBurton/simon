8c93bf33083194853e063fba9d6932dc71aa3f21
Arma Burton <arma@armaburton.com>
2023-10-16 21:38:59 -0700

    {webpack }

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Adjust entry point as per your project
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Adjust output directory as per your project
+    filename: 'bundle.js', // Adjust output filename as per your project
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'], // Presets for React
+          },
+        },
+      },
+    ],
+  },
+};
