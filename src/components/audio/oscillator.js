4f280254a0ef9ae4f000f0c1df6ac74ca07c52c4
Arma Burton <arma@armaburton.com>
2024-07-25 17:09:49 -0700

    {it makes the noise}

--- src/components/audio/oscillator.js
+++ src/components/audio/oscillator.js
@@ -8,10 +8,16 @@ export const audio = (color) => {
 
     const playOscillator = (startTime, endTime, freq) => {
         const oscillator = context.createOscillator();
-        const 
+        const gain = context.createGain();
+
+        oscillator.connect(gain);
+        oscillator.type = 'square';
+        oscillator.frequency.value = freq;
+        gain.gain.value = 0.125;
+        gain.connect(context.destination);
+        oscillator.start(startTime);
+        oscillator.stop(endTime);
     }
 
   playOscillator(context.currentTime, context.currentTime + .42, freq)
-
-
 }
