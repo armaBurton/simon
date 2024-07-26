58d70227d991acfbf89720276c28841e7783226e
Arma Burton <arma@armaburton.com>
2024-07-25 17:07:28 -0700

    {freq}

--- src/components/audio/oscillator.js
+++ src/components/audio/oscillator.js
@@ -1,3 +1,17 @@
 export const audio = (color) => {
-    
+    const context  = new AudioContext();
+
+    const freq = (color === "blue") ? 195.998 :
+    (color === "yellow") ? 261.626 : 
+      (color === "red") ? 329.628 : 
+        (color === "green" ) ? 391.995 : 42;
+
+    const playOscillator = (startTime, endTime, freq) => {
+        const oscillator = context.createOscillator();
+        const 
+    }
+
+  playOscillator(context.currentTime, context.currentTime + .42, freq)
+
+
 }
